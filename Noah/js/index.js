4a11a84d4fb87661ab3c0478d2d56d7adb36de9f
noahfranco <noahfra@gmail.com>
2019-06-27 16:04:13 -0400

    added some CSS styling

--- Noah/js/index.js
+++ Noah/js/index.js
@@ -1,17 +1,22 @@
-// mouseover
+// ----- mouseover -----
 const popBox = document.querySelectorAll(".child-box");
 popBox.forEach(box => {
   box.addEventListener("mouseover", e => {
     box.classList.toggle("large");
   });
-})
-
-
-// ----- Click -----
-const test = document.querySelector(".parent-container1 h1"); 
-test.addEventListener("click", event => {
-    test.textContent = "TeamWork Makes the DreamWork!"
 }); 
+
+
+// ----- mouseout -----
+const test = document.querySelector(".parent-container1 h1"); 
+test.addEventListener("mouseout", event => {
+    test.textContent = "Team Work Makes the Dream Work!"; 
+}); 
+
+
+
+
     
+
 
 
